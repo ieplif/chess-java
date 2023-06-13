9577564af92ddce7a041becbceddc5e003f833e5
Filipe Ribeiro <fszribeiro@gmail.com>
2023-06-13 15:18:51 -0300

    Starting to implement Board and Piece

--- src/App.java
+++ src/App.java
@@ -1,7 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
-        Position pos = new Position(3 , 5);
-        System.out.println(pos);
+        Board board = new Board(8, 8);
     }
 }
